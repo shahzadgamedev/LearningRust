2753c28f75f7753331b460c5a132465e3b0cc8d5
Shahzad Ansari <95477157+shahzadgamedev@users.noreply.github.com>
2025-01-19 12:45:03 +0530

    First Commit

---
+++ src/qunatum_comp.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn simulate_quantum_computer()
+{
+    println!("Welcomeee... : Enter number of Qubits .. ");
+    let mut guess: String = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("You Darn Failed Lol to Maintain Singularity!!!");
+}
