49ee5b6156c6ed7cbe355b6d052e10b3b5cbd655
Shahzad Ansari <95477157+shahzadgamedev@users.noreply.github.com>
2025-01-30 21:44:04 +0530

    Refactor guess game functionality into a separate module

---
+++ src/basics/guess_game.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+use std::io;
+
+pub fn GuessGameLoop()
+{
+    println!("Hello, world!");
+
+    println!("**** Guess Game **** ");
+
+    let computer_guess: u32 = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+        let mut user_guess: String = String::new();
+        println!("\nEnter Your Guess ..");
+        io::stdin()
+            .read_line(&mut user_guess)
+            .expect("Failed to Read Line");
+        println!("you Guessed {}", user_guess);
+
+        let user_guess: u32 = match user_guess
+            .trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please Enter a Valid Number.."); 
+                    continue;
+                }
+            };
+
+        match user_guess.cmp(&computer_guess) {
+            std::cmp::Ordering::Less => println!("Too Small !!"),
+            std::cmp::Ordering::Greater => println!("Too Big !!"),
+            std::cmp::Ordering::Equal => 
+            {
+                println!("Dammmnn ! You Win");
+                break;
+            }
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-use std::io;
-use rand::Rng;
-
 mod qunatum_comp;
+mod basics {
+    pub mod guess_game;
+}
 
 fn main() {
-    println!("Hello, world!");
-
-    println!("**** Guess Game **** ");
-    
-
-    let mut user_guess: String = String::new();
-    let computer_guess = rand::thread_rng().gen_range(1..=100);
-
-
-    io::stdin().read_line(&mut user_guess).expect("Failed to Read Line");
-    println!("you Guessed {}", user_guess);
- 
-
-
+    basics::guess_game::GuessGameLoop();
 }
