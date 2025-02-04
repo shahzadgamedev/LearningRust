b7a71decbb27f97db3907d69e46f60a1fce0c0af
Shahzad Ansari <95477157+shahzadgamedev@users.noreply.github.com>
2025-02-04 15:31:23 +0530

    Update main function to print array elements and comment out guess game loop

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,15 @@ mod basics {
 }
 
 fn main() {
-    basics::guess_game::guess_game_loop();
+  //  basics::guess_game::guess_game_loop();
+
+
+  let arr = [2;4];
+
+  for element in arr {
+        println!("The value of element is: {}", element);
+  }
+
+
+
 }
